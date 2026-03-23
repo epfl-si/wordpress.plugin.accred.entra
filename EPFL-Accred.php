a25a72e25d212fc795019c49d221923ded258c39
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2026-03-23 12:28:01 +0100

    [bump] Version

--- EPFL-Accred.php
+++ EPFL-Accred.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: EPFL Accred Entra
  * Description: Automatically sync access rights to WordPress from EPFL's institutional data repositories
- * Version:     0.17
+ * Version:     0.18
  * Author:      Dominique Quatravaux
  * Author URI:  mailto:dominique.quatravaux@epfl.ch
  */
